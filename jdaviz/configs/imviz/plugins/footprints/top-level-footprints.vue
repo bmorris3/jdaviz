558eb8627e48be9f7a03ffa0a454871f17d60a5f
Brett M. Morris <bmmorris@stsci.edu>
2025-06-11 15:44:48 -0400

    forgotten vue files

---
+++ jdaviz/configs/imviz/plugins/footprints/top-level-footprints.vue
@@ -0,0 +1,15 @@
+<template>
+  <j-tray-plugin
+    :config="config"
+    plugin_key="Footprints"
+    :api_hints_enabled.sync="api_hints_enabled"
+    :description="docs_description"
+    :link="docs_link || 'https://jdaviz.readthedocs.io/en/'+vdocs+'/'+config+'/plugins.html#footprints'"
+    :uses_active_status="uses_active_status"
+    @plugin-ping="plugin_ping($event)"
+    :keep_active.sync="keep_active"
+    :disabled_msg="disabled_msg"
+    :popout_button="popout_button"
+    :scroll_to.sync="scroll_to">
+</j-tray-plugin>
+</template>
